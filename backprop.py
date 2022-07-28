fd53aa4f2c608e0860924df722ec80a6ea95985b
Creatune <rjjoshixyz@gmail.com>
2022-07-28 17:00:42 +0530

    finished backprop for a group of neurons

--- backprop.py
+++ backprop.py
@@ -87,3 +87,111 @@ print(y)  # this was previously 6, but now 5.969, which means that we successful
 # to decrease how wrong our model is, which means we would have to take the derivative of the loss function
 
 # ALL of the above has to be applied to each and every neuron in a neural network
+
+# backprop for a set of neurons:
+import numpy as np
+
+# Passed in gradient from the next layer
+dvalues = np.array([[1., 1., 1.],
+                    [2., 2., 2.],
+                    [3., 3., 3.]])
+
+# We have 3 sets of weights - one set for each neuron
+# we have 4 inputs, thus 4 weights
+# recall that we keep weights transposed
+
+weights = np.array([[0.2, 0.8, -0.5, 1],
+                    [0.5, -0.91, 0.26, -0.5],
+                    [-0.26, -0.27, 0.17, 0.87]]).T
+
+# sum weights of given input and multiply by the passed in gradient for this neuron
+dinputs = np.dot(dvalues, weights.T)
+
+print(dinputs)  # dinputs is a gradient of the neuron function with respect to inputs
+
+# We have 3 sets of inputs - samples
+inputs = np.array([[1, 2, 3, 2.5],
+                   [2., 5., -1., 2],
+                   [-1.5, 2.7, 3.3, -0.8]])
+# sum weights of given input
+# and multiply by the passed in gradient for this neuron
+dweights = np.dot(inputs.T, dvalues)
+
+print(dweights)
+
+# One bias for each neuron
+# biases are the row vector with a shape (1, neurons)
+biases = np.array([[2, 3, 0.5]])
+
+# dbiases - sum values, do this over samples (first axis), keepdims
+# since this by default will produce a plain list -
+# we explained this in the chapter 4
+dbiases = np.sum(dvalues, axis=0, keepdims=True)
+print(dbiases)
+
+# Example layer output
+z = np.array([[1, 2, -3, -4],
+              [2, -7, -1, 3],
+              [-1, 2, 5, -1]])
+# ReLU activation's derivative
+drelu = np.zeros_like(z)
+drelu[z > 0] = 1
+
+print(drelu)
+
+# The chain rule
+dvalues = np.array([[1, 2, 3, 4],
+                    [5, 6, 7, 8],
+                    [9, 10, 11, 12]])
+drelu *= dvalues
+print(drelu)
+
+# doing a full backprop for a set of neurons:
+
+
+# Passed in gradient from the next layer
+# for the purpose of this example we're going to use
+# an array of an incremental gradient values
+dvalues = np.array([[1., 1., 1.],
+                    [2., 2., 2.],
+                    [3., 3., 3.]])
+# We have 3 sets of inputs - samples
+inputs = np.array([[1, 2, 3, 2.5],
+                   [2., 5., -1., 2],
+                   [-1.5, 2.7, 3.3, -0.8]])
+# We have 3 sets of weights - one set for each neuron
+# we have 4 inputs, thus 4 weights
+# recall that we keep weights transposed
+weights = np.array([[0.2, 0.8, -0.5, 1],
+                    [0.5, -0.91, 0.26, -0.5],
+                    [-0.26, -0.27, 0.17, 0.87]]).T
+# One bias for each neuron
+# biases are the row vector with a shape (1, neurons)
+biases = np.array([[2, 3, 0.5]])
+
+# forward pass:
+layer_outputs = np.dot(inputs, weights) + biases
+
+relu_outputs = np.maximum(layer_outputs, 0)
+
+# Let's optimize and test backpropagation here
+# ReLU activation - simulates derivative with respect to input values
+# from next layer passed to current layer during backpropagation
+drelu = relu_outputs.copy()
+drelu[layer_outputs <= 0] = 0
+
+# Dense layer
+# dinputs - multiply by weights
+dinputs = np.dot(drelu, weights.T)
+# dweights - multiply by inputs
+dweights = np.dot(inputs.T, drelu)
+# dbiases - sum values, do this over samples (first axis), keepdims
+# since this by default will produce a plain list -
+# we explained this in the chapter 4
+dbiases = np.sum(drelu, axis=0, keepdims=True)
+
+# Update parameters
+weights += -0.001 * dweights
+biases += -0.001 * dbiases
+print(weights)
+print(biases)
