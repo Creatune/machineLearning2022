57660e2cb19202a3b329aa9a422f30f19bfe560f
Creatune <rjjoshixyz@gmail.com>
2022-08-01 17:55:48 +0530

    Created stochastic gradient descent class with learning rate set by default to 1.0

---
+++ optimizers.py
@@ -0,0 +1,8 @@
+class optimizer_SGD:
+
+    def __init__(self, learning_rate=1.0):
+        self.learning_rate = learning_rate
+
+    def update_params(self, layer):
+        layer.weights += self.learning_rate * layer.dweights
+        layer.biases += self.learning_rate * layer.dbiases
