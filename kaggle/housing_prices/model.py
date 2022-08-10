3e70263383bf986bb778375dab580912cc5b862e
Creatune <rjjoshixyz@gmail.com>
2022-08-10 15:59:51 +0530

    shitty solution for housing prices

--- kaggle/housing_prices/model.py
+++ kaggle/housing_prices/model.py
@@ -1,16 +1,22 @@
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import mean_squared_error
 import pandas as pd
+import numpy as np
 
-train_X = pd.read_csv('train.csv')
-train_y = pd.read_csv('train.csv').SalePrice
-
-val_X = pd.read_csv('test.csv')
-
-model = RandomForestClassifier(random_state=1)
-print('SalePrice' in val_X)
+home_data_train = pd.read_csv('train.csv')
+features = ['LotArea', '1stFlrSF', '2ndFlrSF', 'TotalBsmtSF']
+home_data_test = pd.read_csv('test.csv')
+train_X = home_data_train[features]
+print(train_X.head())
+train_y = home_data_train.SalePrice
+test_X = home_data_test[features]
+test_X = test_X.dropna(axis=0)
+model = RandomForestClassifier()
 model.fit(train_X, train_y)
+y_pred = model.predict(test_X)
+print(y_pred)
 
-y_pred = model.predict(val_X)
-
-# mean_squared_error(y_pred, val_y)
+IDs = np.array([i for i in range(1461, 1461 * 2 - 3)])
+res = pd.DataFrame(y_pred)
+res.index += 1461
+res.columns = ['SalePrice']
+res.to_csv('predictions.csv')
