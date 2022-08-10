9de88df3ff40d54ea2667b920ea1d3c6077f1c47
Creatune <rjjoshixyz@gmail.com>
2022-08-10 16:40:23 +0530

    improved model and changed the way i write into csv files

--- kaggle/housing_prices/housing_prices_model.py
+++ kaggle/housing_prices/housing_prices_model.py
@@ -1,4 +1,4 @@
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
 import pandas as pd
 
 home_data_train = pd.read_csv('train.csv')
@@ -9,13 +9,15 @@ print(train_X.head())
 
 train_y = home_data_train.SalePrice
 test_X = home_data_test[features]
-test_X = test_X.fillna(0, axis=0)
-model = RandomForestClassifier()
+
+print(test_X.describe())
+
+test_X = test_X.fillna(3086, axis=0)
+model = RandomForestRegressor()
 model.fit(train_X, train_y)
 y_pred = model.predict(test_X)
 print(y_pred)
 
-res = pd.DataFrame(y_pred)
-res.index += 1461
-res.columns = ['SalePrice']
-res.to_csv('predictions.csv')
+output = pd.DataFrame({'Id': home_data_test.Id,
+                       'SalePrice': y_pred})
+output.to_csv('predictions.csv', index=False)
